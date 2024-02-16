8e29b19f1f427ab8c969fae6f42a8b346d4ee971
msiquera-dev <marcusviniciusb.siqueira@hotmail.com>
2024-02-15 21:09:04 -0300

    Python program that has example and explanation of how variable works

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# 02-python_variables
- 
+# Python: Variables
+
+[PT]: Esse é um exemplo de um programa em Python que ensina variaveis
+
+[EN]: This is an example of a Python program that teachs variables
+
+Link: 
---
+++ variable.py
@@ -0,0 +1,15 @@
+# PT-BR: 
+#   Isso é um comentário e será ignorado pelo Python
+#   Esse é um exemplo de um programa em Python que ensina como variáveis funcionam
+# EN: 
+#   This is a comment and will be ignored by Python.
+#   This is an example of a Python program that show how variables works
+
+value = 1
+print(value)
+value = 2
+print(value)
+value_second = None
+print(value_second)
+value_second = 'A Value'
+print(value_second)
