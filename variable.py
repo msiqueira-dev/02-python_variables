4472ca3ff1b2daa50ec23363a2a993827ded7093
msiquera-dev <marcusviniciusb.siqueira@hotmail.com>
2024-02-20 20:14:18 -0300

    Adicionando comentários e removendo arquivo desnecessário

--- variable.py
+++ variable.py
@@ -5,11 +5,22 @@
 #   This is a comment and will be ignored by Python.
 #   This is an example of a Python program that show how variables works
 
+# PT-BR: cria uma variavel de nome value iniciando ela com o valor numérico 1
+# EN: creates a variable with the name value and initializes it with value number 1
 value = 1
 print(value)
+
+# PT-BR: Atribui valor numérico 2 a mesma variável de nome value, substituindo o anterior
+# EN: Assign the numeric value 2 to the same variable with the name value, replacing the old one
 value = 2
 print(value)
+
+# PT-BR: Atribui valor None (vazio) a mesma variável de nome value, substituindo o anterior
+# EN: Assign the value None (empty) to the same variable with the name value, replacing the old one
 value_second = None
 print(value_second)
+
+# PT-BR: Atribui valor A value (string) a mesma variável de nome value, substituindo o anterior
+# EN: Assign the value A value (string) to the same variable with the name value, replacing the old one
 value_second = 'A Value'
 print(value_second)
